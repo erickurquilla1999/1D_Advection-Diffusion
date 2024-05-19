39a3394d2b398b06d467a3617091ff35098d8197
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-05-19 08:54:50 -0400

    create input file

---
+++ inputs.py
@@ -0,0 +1,14 @@
+# Physical domain 
+x_initial = 0 # (m) initial domain coordinate
+x_final = 10 # (m) final domain coordinate
+
+# DG method
+N_elements= 16 # number of elements
+p_basis_order = 3 # lagrange basis order
+
+# Gauss cuadrature
+n_gauss_poins = 20 
+
+# Governing equation parameters
+a = 1
+v = 1
