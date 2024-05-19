04b7ebb141bb23ebc81b9b56e969d0e93cd363f9
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-05-19 16:20:05 -0400

    update input parameters

--- inputs.py
+++ inputs.py
@@ -1,14 +1,18 @@
 # Physical domain 
 x_initial = 0 # (m) initial domain coordinate
-x_final = 10 # (m) final domain coordinate
+x_final = 1 # (m) final domain coordinate
 
 # DG method
-N_elements= 16 # number of elements
-p_basis_order = 3 # lagrange basis order
+N_elements = 3 # number of elements
+p_basis_order = 1 # lagrange basis order
 
 # Gauss cuadrature
-n_gauss_poins = 20 
+n_gauss_poins = 5 
 
 # Governing equation parameters
 a = 1
 v = 1
+
+# Boundary conditions
+u_at_x_initial = 0
+u_at_x_final = 1
