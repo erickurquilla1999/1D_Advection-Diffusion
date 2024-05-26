30315fb371cfa1f554e750aa78b3a282ffe0084f
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-05-26 14:28:08 -0400

    include Pe for calculations

--- inputs.py
+++ inputs.py
@@ -3,15 +3,16 @@ x_initial = 0 # (m) initial domain coordinate
 x_final = 1 # (m) final domain coordinate
 
 # DG method
-N_elements = 3 # number of elements
-p_basis_order = 1 # lagrange basis order
+N_elements = 4 # number of elements
+p_basis_order = 2 # lagrange basis order
 
 # Gauss cuadrature
 n_gauss_poins = 5 
 
 # Governing equation parameters
 a = 1
-v = 1
+Pe = 100
+v = a * x_final / Pe
 
 # Boundary conditions
 u_at_x_initial = 0
