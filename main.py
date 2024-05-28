9c6a4cad047bad45551549c835c7ea5e422a6669
erickurquilla1999 <erickurquillla1999@gmail.com>
2024-05-28 13:01:39 -0400

    compute error

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import plots
 element_number, left_node_coordinates, right_node_coordinates, nodes_coordinates_phys_space, nodes_coordinates_ref_space, element_lengths = grid_generation.generate_1d_mesh(inputs.x_initial,inputs.x_final,inputs.N_elements,inputs.p_basis_order)
 
 # generating reference space information
-gauss_weights, basis_values_at_gauss_quad, basis_values_x_derivative_at_gauss_quad, basis_values_at_nodes = basis.generate_reference_space(element_number,nodes_coordinates_phys_space,inputs.n_gauss_poins,left_node_coordinates, right_node_coordinates)
+gauss_weights, basis_values_at_gauss_quad, basis_values_x_derivative_at_gauss_quad, basis_values_at_nodes, gauss_coords_phys_space = basis.generate_reference_space(element_number,nodes_coordinates_phys_space,inputs.n_gauss_poins,left_node_coordinates, right_node_coordinates)
 
 # solving using continous galerkin method
 cg_solution = evolve.CG_solver(element_number, element_lengths, gauss_weights, basis_values_at_gauss_quad, basis_values_x_derivative_at_gauss_quad)
@@ -22,4 +22,4 @@ mass_matrix_1_inverse = evolve.compute_mass_matrix_1_inverse(element_number, ele
 dg_solution = evolve.DG_solver_advection(element_number, element_lengths, gauss_weights, basis_values_at_gauss_quad, basis_values_x_derivative_at_gauss_quad, nodes_coordinates_phys_space, mass_matrix_1_inverse)
 
 # plot solution
-plots.plot(cg_solution, dg_solution, nodes_coordinates_phys_space)
+plots.plot(cg_solution, dg_solution, nodes_coordinates_phys_space, basis_values_at_gauss_quad, gauss_coords_phys_space, gauss_weights)
--- plots.py
+++ plots.py
@@ -20,20 +20,60 @@ mpl.rcParams['ytick.minor.size'] = 4
 mpl.rcParams['ytick.minor.width'] = 2
 mpl.rcParams['axes.linewidth'] = 2
 
-def plot(cg_solution, dg_solution, nodes_coordinates_phys_space):
+def plot(cg_solution, dg_solution, nodes_coordinates_phys_space, basis_values_at_gauss_quad, gauss_coords_phys_space, gauss_weights):
 
-    coords_for_plot = np.unique(np.array(nodes_coordinates_phys_space).flatten())
-    coords_exact_solution = np.linspace(inputs.x_initial, inputs.x_final, 100)
-    exact_solution = ( 1 - np.exp( inputs.a * coords_exact_solution / inputs.v ) ) / ( 1 - np.exp( inputs.a / inputs.v ) )
+    ###############################################
+    # Compute error
     dg_cords = nodes_coordinates_phys_space
     dg_solut = dg_solution.reshape(inputs.N_elements, inputs.p_basis_order + 1)
 
+    cg_solut = []
+    for i in range(inputs.N_elements):
+        cg_solut.append(list(cg_solution[ i * inputs.p_basis_order : i * inputs.p_basis_order + inputs.p_basis_order + 1]))
+
+    cg_quadrature = []
+    dg_quadrature = []
+    for i in range( inputs.N_elements ):
+        cg_quadrature.append( basis_values_at_gauss_quad[i] @ cg_solut[i] )
+        dg_quadrature.append( basis_values_at_gauss_quad[i] @ dg_solut[i] )
+
+    w = ( 1 - np.cos( np.pi * gauss_coords_phys_space / inputs.x_final) ) / 2
+    es_quadrature = ( 1 - np.exp( inputs.a * gauss_coords_phys_space / inputs.v ) ) / ( 1 - np.exp( inputs.a / inputs.v ) )
+
+    # weighted error
+    cg_weighted_error = 0 
+    dg_weighted_error = 0 
+    for i in range( inputs.N_elements ):
+        cg_weighted_error += np.abs( 0.5 * np.sum( gauss_weights[i] * w[i] * ( cg_quadrature[i] - es_quadrature[i] ) ) )
+        dg_weighted_error += np.abs( 0.5 * np.sum( gauss_weights[i] * w[i] * ( dg_quadrature[i] - es_quadrature[i] ) ) )
+
+    # l2 error
+    cg_l2_error = 0 
+    dg_l2_error = 0 
+    for i in range( inputs.N_elements ):
+        cg_l2_error += np.sqrt( 0.5 * np.sum( gauss_weights[i] * ( cg_quadrature[i] - es_quadrature[i] ) ** 2 ) )
+        dg_l2_error += np.sqrt( 0.5 * np.sum( gauss_weights[i] * ( dg_quadrature[i] - es_quadrature[i] ) ** 2 ) )
+
+    ###############################################
+    # Plot solution
+    
     fig, ax = plt.subplots()
-    ax.plot(coords_for_plot, cg_solution, label = 'CG', color = 'C0')
+
+    # cg solution
+    cg_cords = np.unique(np.array(nodes_coordinates_phys_space).flatten())
+    ax.plot(cg_cords, cg_solution, label = 'CG', color = 'C0')
+
+    # dg solution
     for i in range(inputs.N_elements):
         label = 'DG' if i == 0 else None
         ax.plot(dg_cords[i], dg_solut[i], color='C1', label=label)
+
+    # exact solution
+    coords_exact_solution = np.linspace(inputs.x_initial, inputs.x_final, 100)
+    exact_solution = ( 1 - np.exp( inputs.a * coords_exact_solution / inputs.v ) ) / ( 1 - np.exp( inputs.a / inputs.v ) )
     ax.plot(coords_exact_solution, exact_solution, linestyle = 'dotted', label = 'Exact', color = 'black')
+
+    # plot setting
     leg = ax.legend(framealpha=0.0,ncol=1)
     leg.get_frame().set_edgecolor('w')
     leg.get_frame().set_linewidth(0.0)
@@ -41,3 +81,30 @@ def plot(cg_solution, dg_solution, nodes_coordinates_phys_space):
     ax.set_ylabel(r'$u(x)$')
     fig.savefig('solution.pdf',bbox_inches='tight')
     plt.clf()
+
+    ###############################################
+    # Print information
+
+    dg_cords = list(dg_cords)
+    dg_solut = list(dg_solut)
+    for i in range(inputs.N_elements):
+        dg_cords[i] = list(dg_cords[i])
+        dg_solut[i] = list(dg_solut[i])
+
+    print(f'#Ne={inputs.N_elements}')
+    print(f'#p={inputs.p_basis_order}')
+    print(f'#Pe={inputs.Pe}')
+
+    print(f'cg_l2_error_{inputs.N_elements}_{inputs.p_basis_order}_{inputs.Pe} = {cg_l2_error}')
+    print(f'dg_l2_error_{inputs.N_elements}_{inputs.p_basis_order}_{inputs.Pe} = {dg_l2_error}')
+    print(f'cg_weighted_error_{inputs.N_elements}_{inputs.p_basis_order}_{inputs.Pe} = {cg_weighted_error}')
+    print(f'dg_weighted_error_{inputs.N_elements}_{inputs.p_basis_order}_{inputs.Pe} = {dg_weighted_error}')
+    
+    print(f'cg_cords_{inputs.N_elements}_{inputs.p_basis_order}_{inputs.Pe} = {list(cg_cords)}')
+    print(f'cg_solution_{inputs.N_elements}_{inputs.p_basis_order}_{inputs.Pe} = {list(cg_solution)}')
+    
+    print(f'dg_cords_{inputs.N_elements}_{inputs.p_basis_order}_{inputs.Pe} = {dg_cords}')
+    print(f'dg_solut_{inputs.N_elements}_{inputs.p_basis_order}_{inputs.Pe} = {dg_solut}')
+
+    print(f'coords_exact_solution_{inputs.N_elements}_{inputs.p_basis_order}_{inputs.Pe} = {list(coords_exact_solution)}')
+    print(f'exact_solution_{inputs.N_elements}_{inputs.p_basis_order}_{inputs.Pe} = {list(exact_solution)}')
